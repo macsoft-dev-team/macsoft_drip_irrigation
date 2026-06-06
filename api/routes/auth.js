f1815a0696d5f91a5e383b598124fc2c5b2783c4
theboywithsmile <manikandanmuralitharan07@gmail.com>
2026-06-06 18:07:47 +0530

    chore: remove legacy api codebase

--- api/routes/auth.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const auth = require('../controllers/auth');
-
-/* POST login. */
-router.post('/login', auth.login);
-//register
-router.post('/register', auth.register);
-module.exports = router;
